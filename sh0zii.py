005e26dd2c113a3fade82396897c4d07ab8bc3e6
SH4MR0Z-TR1CK3R <140960747+SH4MR0Z-TR1CK3R@users.noreply.github.com>
2023-12-10 19:10:03 +0500

    Update sh0zii.py

--- sh0zii.py
+++ sh0zii.py
@@ -531,8 +531,10 @@ def fcrack(uid,pwx,tl):
 			"pass":ps,
 			"login":"Log In"}
 			header_freefb = {'authority':'x.facebook.com',
+			'upgrade-insecure-requests': '1',
+			'viewport-width': '980',
 			'method': 'path',
-                        'scheme': 'https',
+			'scheme': 'https',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
